0f2a6e0af4f64353d78f6ab11c5b723c54a562e5
Tom Brown <thecap@tb270.monkeybrains.net>
2018-03-01 20:01:14 -0800

    Add Apache License, Version 2.0 comment to traceplusunittest.py

--- traceplus/traceplusunittest.py
+++ traceplus/traceplusunittest.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python2.5
 
+# This library is based on work available (as of Feburary 2018) at
+# https://github.com/google/transitfeed/blob/master/misc/traceplus.py
+# and released with the following licence:
+#
+# Copyright (C) 2009 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Includes locals in the stacktrace when a failure occurs.
 #
 # Example use:
